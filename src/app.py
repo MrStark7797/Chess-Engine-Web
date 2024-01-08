756e5105010038bafe22b298b597fe1d645b0890
MrStark7797 <starkiewill@gmail.com>
2024-01-08 10:02:25 +0000

    working

--- src/app.py
+++ src/app.py
@@ -5,7 +5,21 @@
 # packages
 from flask import Flask
 from flask import render_template
+from flask import request
+import chess
+import chess.engine
+import chess.pgn
+import io
+import random
+from flask import jsonify
+from flask import Response
+from flask_pymongo import PyMongo
+from datetime import datetime
+import json
+import asyncio
 
+
+engine = chess.engine.SimpleEngine.popen_uci('./engine/slice.exe')
 # create web app instance
 app = Flask(__name__)
 
@@ -13,6 +27,25 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('Analysis.html')
+# make move API
+@app.route('/make_move', methods=['POST'])
+def make_move():
+    # extract FEN string from HTTP POST request body
+    fen = request.form.get('fen')
+
+    # init python chess board instance
+    board = chess.Board(fen)
+    
+    # search for best move
+    result = engine.play(board, chess.engine.Limit(depth=6))
+    
+    # update internal python chess board state
+    board.push(result.move)
+    
+    # extract FEN from current board state
+    fen = board.fen()
+    
+    return {'fen': fen}
 
 # main driver
 if __name__ == '__main__':
